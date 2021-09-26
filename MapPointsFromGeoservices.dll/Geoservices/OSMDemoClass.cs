cd6bbaf15949ed4da58148ff71ae9cdfdce51353
Denis <Denis@Gritsyuk.ru>
2021-09-26 12:43:40 +0300

    Refactoring

---
+++ MapPointsFromGeoservices.dll/Geoservices/OSMDemoClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MapPointsFromGeoservices
+{
+    class OSMDemoClass : OpenStreetMap
+    {
+        public OSMDemoClass(string address, string filePath) : base(address, filePath) { }
+
+        internal override MapPoint[] GetRegionMapPoints() => GetMapPointArray(new Uri(Address));
+    }
+}
---
+++ MapPointsFromGeoservices.dll/MapPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace MapPointsFromGeoservices
+{
+    [DataContract]
+    public struct MapPoint
+    {
+        public MapPoint(double x, double y)
+        {
+            this._x = this._y = 0;
+            this._x = x;
+            this._y = y;
+        }
+
+        [DataMember(Name = "X")]
+        private double _x;
+        public double X { get => _x; }
+
+        [DataMember(Name = "Y")]
+        private double _y;
+        public double Y { get => _y; }
+
+        public override string ToString() => $"MapPoint({this._x}, {this._x})";
+    }
+}
